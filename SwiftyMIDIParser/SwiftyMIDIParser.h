3a768200e7304a5ad0d98de64e2e4729b28ca18b
Kainosuke OBATA <phisz.drums@gmail.com>
2019-10-04 18:05:47 +0900

    initial commit

---
+++ SwiftyMIDIParser/SwiftyMIDIParser.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyMIDIParser.h
+//  SwiftyMIDIParser
+//
+//  Created by kai on 2019/10/04.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyMIDIParser.
+FOUNDATION_EXPORT double SwiftyMIDIParserVersionNumber;
+
+//! Project version string for SwiftyMIDIParser.
+FOUNDATION_EXPORT const unsigned char SwiftyMIDIParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyMIDIParser/PublicHeader.h>
+
+
