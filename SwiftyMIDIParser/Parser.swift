ab1f967422a1222d23ba3b6f1bb2f1eea89840e1
Kainosuke OBATA <phisz.drums@gmail.com>
2019-10-07 00:48:16 +0900

    refactor Parser: delete Segmentator

--- SwiftyMIDIParser/Parser.swift
+++ SwiftyMIDIParser/Parser.swift
@@ -10,29 +10,7 @@ import Foundation
 
 public class Parser {
     public func input(data: [UInt8]) {
-        data.forEach { byte in
-            let statusOrNil = StatusType.fromByte(byte)
-            if let status = statusOrNil {
-                if status.isSystemRealTimeMessage {
-                    notifier.notify(messageData: [byte])
-                    return
-                }
-            }
-            if !parsing {
-                segmentator.reset()
-                if statusOrNil != nil  {
-                    lastStatus = byte
-                } else {
-                    // if `RunningStatus`, we have to put last status byte.
-                    _ = segmentator.input(byte: lastStatus!)
-                }
-                parsing = true
-            }
-            if let message = segmentator.input(byte: byte) {
-                parsing = false
-                notifier.notify(messageData: message)
-            }
-        }
+        data.forEach(parseByte)
     }
     
     public init() {
@@ -40,9 +18,87 @@ public class Parser {
     }
     
     private var parsing: Bool = false
-    private var lastStatus: UInt8?
-    private let segmentator: Segmentator = Segmentator()
+    private var lastStatus: StatusType?
+    private var lastStatusByte: UInt8?
     public let notifier: Notifier = Notifier()
+    
+    private var parsingData: [UInt8] = []
+    
+    private func parseByte(_ byte: UInt8) {
+        let statusOrNil = StatusType.fromByte(byte)
+        if let status = statusOrNil, status.isSystemRealTimeMessage {
+            // System Real Time Message is 1 byte and can interrupt any messages.
+            // So we'll notify this Real Time Message immediately and keep current status.
+            notifier.notify(messageData: [byte])
+            return
+        }
+        
+        switch (parsing, lastStatus, statusOrNil) {
+        case (true, .systemExclusive, .endOfExclusive):
+            // parsing exclusive and receive end of exclusive -> notify
+            parsingData.append(byte)
+            notifier.notify(messageData: parsingData)
+            clearData()
+            
+        case (true, nil, _):
+            // impossible condition
+            fatalError()
+        
+        case (false, nil, nil):
+            // Received unkown byte before receiving StatusByte. So we'll ignore this byte.
+            return
+        
+        case (false, nil, .some(_)):
+            // Received 1st status. So start parsing 1st message.
+            gotMessageByte(status: byte, data: nil)
+            
+        case (false, .some(_), nil):
+            // Maybe running status. So start parse with previous status.
+            gotMessageByte(status: lastStatusByte!, data: byte)
+            
+        case (false, .some(_), .some(_)):
+            // Received new status. So start parse with new status.
+            gotMessageByte(status: byte, data: nil)
+        
+        case (true, .some(_), nil):
+            // Received new data byte.
+            gotMessageByte(status: nil, data: byte)
+            
+        case (true, .some(_), .some(_)):
+            // Receiving new Status while parsing current Status.
+            // This indicates the sequence is invalid.
+            // Obviously this is MIDI protocol violation. But we'll clear current status
+            // and continue to parse new status instead of crash.
+            // Or, We can consider some handler to notify protocol violation detection...
+            clearData()
+            gotMessageByte(status: byte, data: nil)
+        }
+    }
+    
+    private func clearData() {
+        parsingData = []
+        parsing = false
+    }
+    
+    private func gotMessageByte(status: UInt8?, data: UInt8?) {
+        parsing = true
+        
+        if let status = status {
+            lastStatusByte = status
+            lastStatus = StatusType.fromByte(status)!
+            parsingData.append(status)
+        }
+        if let data = data {
+            parsingData.append(data)
+        }
+        
+        if case .fixed(let dataSize) = lastStatus!.dataByteSize {
+            if (parsingData.count - 1) == dataSize {
+                notifier.notify(messageData: parsingData)
+                clearData()
+            }
+        }
+    }
 }
 
 
